1b5de05b1c4d39957a7c400979b4fc199ed2509b
jennyf19 <ferriesj@gmail.com>
2016-09-16 18:09:41 -0700

    added async methods

---
+++ GitApi1.5/AsyncClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Octokit;
+using System.Net.Http;
+
+namespace GitApi1._5
+{
+    class AsyncClass
+    {
+        public async Task GitApi()
+        {
+            await Task.Run(async () =>
+            {
+                await LongRunningOperation();
+            });
+        }
+        private static async Task LongRunningOperation()
+        {
+            var owner = string.Empty;
+            var reponame = string.Empty;
+
+            owner = "jennnyf19";
+            reponame = "BinaryTree";
+
+            Console.WriteLine(owner + " " + reponame);
+
+            GitHubClient client = new GitHubClient(new ProductHeaderValue("JennysAwesomeGitRelease"));
+
+            Console.WriteLine(client);
+
+            await Task.Run(async () =>
+            {
+                var repository = await client.Repository.Get(owner, reponame);
+
+
+                Console.WriteLine(repository);
+
+                Console.WriteLine(String.Format("binaryTree can be found at {0}\n", repository.HtmlUrl));
+
+                Console.WriteLine("It currently has {0} watchers and {1} forks\n", repository.StargazersCount, repository.ForksCount);
+
+                Console.WriteLine("It has {0} open issues\n", repository.OpenIssuesCount);
+
+                Console.WriteLine("And GitHub thinks it is a {0} project", repository.Language);
+            });
+
+
+        }
+    }
+}
--- GitApi1.5/Program.cs
+++ GitApi1.5/Program.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Octokit;
+using System.Net;
+using Octokit.Reactive;
+using System.Net.Http;
 
 namespace GitApi1._5
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var demo = new AsyncClass();
+            demo.GitApi();
+
+            while (false)
+            {
+                Console.WriteLine("doing stuff on the main thread...........................");
+            }
+            Console.ReadLine();
         }
+
     }
+
 }
